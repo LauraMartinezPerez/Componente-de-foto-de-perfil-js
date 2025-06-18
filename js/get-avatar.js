093c0e78a99a02ce8b7827c9f5fc88c97814a7d8
Laura Martinez Perez <laura.laukiz@gmail.com>
2025-06-18 21:41:45 +0200

    Update get-avatar.js

--- js/get-avatar.js
+++ js/get-avatar.js
@@ -34,6 +34,30 @@ function writeImage() {
     profileImage.style.backgroundImage = `url(${fr.result})`;
     profilePreview.style.backgroundImage = `url(${fr.result})`;
 
+ /*Crear un nuevo elemento (POST)
+Para crear un nuevo registro y almacenar los datos ingresados por la usuaria en el formulario para generar una nueva tarjeta, es necesario realizar una solicitud HTTP de tipo POST. 
+Este método envía la información al servidor, donde se procesa y guarda en la base de datos, creando así un nuevo recurso. 
+Nosotras, desde el frontend, debemos realizar la petición al servidor con la ruta https://dev.adalab.es/api/info/data.
+
+Ejemplo de JSON completo que debemos enviar al servidor:
+
+Copy
+{
+  "field1": 0,
+  "field2": "",
+  "field3": "",
+  "field4": "",
+  "field5": "",
+  "field6": "",
+  "field7": "",
+  "photo": ""
+}
+Para en este gcaso guardar la foto/imagen en el objeto hay que poner: 
+formData.photo = fr.result;
+formData sería el nombre de la varibale de nuestro objeto
+*/
+
+
     /* Si en lugar de establecer la imagen como fondo de un elemento, 
       estás trabajando con una etiqueta <img> en el HTML, entonces en vez de 
       asignar la imagen como background, debes establecer la URL en el atributo `src` de la imagen.
